4701239fedb86d37e93cc47e974525082a5482c1
David Ferreira <davidferreira87@gmail.com>
2024-04-27 17:44:25 -0300

    Movimientos de histórico

--- proyecto 2/proyecto 2/Form1.cs
+++ proyecto 2/proyecto 2/Form1.cs
@@ -19,82 +19,82 @@ namespace proyecto_2
             InitializeComponent();
         }
 
-public class ConexionBD
-    {
-        public SqlConnection conexion;
-        public string connectionString = "server=desktop-ukems2i\\sqlexpress; database=MiBaseDeDatos; integrated security=true";
+        public class ConexionBD
+        {
+            public SqlConnection conexion;
+            public string connectionString = "server=desktop-ukems2i\\sqlexpress; database=MiBaseDeDatos; integrated security=true";
 
 
             public ConexionBD()
-        {
-            conexion = new SqlConnection(connectionString);
-        }
+            {
+                conexion = new SqlConnection(connectionString);
+            }
 
             public SqlConnection AbrirConexion()
-        {
-            if (conexion.State == System.Data.ConnectionState.Closed)
             {
-                conexion.Open();
+                if (conexion.State == System.Data.ConnectionState.Closed)
+                {
+                    conexion.Open();
+                }
+                return conexion;
             }
-            return conexion;
-        }
 
-        public SqlConnection CerrarConexion()
-        {
-            if (conexion.State == System.Data.ConnectionState.Open)
+            public SqlConnection CerrarConexion()
             {
-                conexion.Close();
+                if (conexion.State == System.Data.ConnectionState.Open)
+                {
+                    conexion.Close();
+                }
+                return conexion;
             }
-            return conexion;
         }
-    }
 
         private void btnMostrar_Click(object sender, EventArgs e)
         {
-            
-                DataTable dt = new DataTable();
-                ConexionBD conexionBD = new ConexionBD();
-                using (SqlConnection conexion = conexionBD.AbrirConexion())
-                {
-                    SqlDataAdapter adapter = new SqlDataAdapter("select * from Estudiantes", conexion);
-                    adapter.SelectCommand.CommandType = CommandType.Text;
-                    adapter.Fill(dt);
-                    dataGridView1.DataSource = dt;
-                }
-            
+
+            DataTable dt = new DataTable();
+            ConexionBD conexionBD = new ConexionBD();
+            using (SqlConnection conexion = conexionBD.AbrirConexion())
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter("select * from Estudiantes", conexion);
+                adapter.SelectCommand.CommandType = CommandType.Text;
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+
         }
 
         private void btnCerrar_Click(object sender, EventArgs e)
         {
-          Close();
+            Close();
         }
 
         private void btnModificar_Click(object sender, EventArgs e)
-        { 
-                int id = Convert.ToInt32(txtId.Text); 
-                string nuevoNombre = txtNombre.Text; 
-                string nuevoApellido = txtApellido.Text; 
-                DateTime nuevaFechaNacimiento = dtpFecha.Value; 
+        {
+            int id = Convert.ToInt32(txtId.Text);
+            string nuevoNombre = txtNombre.Text;
+            string nuevoApellido = txtApellido.Text;
+            DateTime nuevaFechaNacimiento = dtpFecha.Value;
 
-                string query = "UPDATE Estudiantes SET Nombre = @Nombre, Apellido = @Apellido, FechaNacimiento = @FechaNacimiento WHERE Id = @Id";
-                ConexionBD conexionBD = new ConexionBD();
-                using (SqlConnection conexion = conexionBD.AbrirConexion())
-                {
-                    
-                    SqlCommand cmd = new SqlCommand(query, conexion);
-                    cmd.Parameters.AddWithValue("@Nombre", nuevoNombre);
-                    cmd.Parameters.AddWithValue("@Apellido", nuevoApellido);
-                    cmd.Parameters.AddWithValue("@FechaNacimiento", nuevaFechaNacimiento);
-                    cmd.Parameters.AddWithValue("@Id", id);
-                }
+            string query = "UPDATE Estudiantes SET Nombre = @Nombre, Apellido = @Apellido, FechaNacimiento = @FechaNacimiento WHERE Id = @Id";
+            ConexionBD conexionBD = new ConexionBD();
+            using (SqlConnection conexion = conexionBD.AbrirConexion())
+            {
+
+                SqlCommand cmd = new SqlCommand(query, conexion);
+                cmd.Parameters.AddWithValue("@Nombre", nuevoNombre);
+                cmd.Parameters.AddWithValue("@Apellido", nuevoApellido);
+                cmd.Parameters.AddWithValue("@FechaNacimiento", nuevaFechaNacimiento);
+                cmd.Parameters.AddWithValue("@Id", id);
+            }
 
         }
 
         private void btnInsertar_Click(object sender, EventArgs e)
         {
 
-            string nombre = txtNombre.Text; 
-            string apellido = txtApellido.Text; 
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
             DateTime fechaNacimiento = dtpFecha.Value;
 
             string query = "INSERT INTO Estudiantes (Nombre, Apellido, FechaNacimiento) VALUES (@Nombre, @Apellido, @FechaNacimiento)";
@@ -112,7 +112,7 @@ public class ConexionBD
 
         private void btnEliminar_Click(object sender, EventArgs e)
         {
-            int id = Convert.ToInt32(txtId.Text); 
+            int id = Convert.ToInt32(txtId.Text);
 
             string query = "DELETE FROM Estudiantes WHERE Id = @Id";
             ConexionBD conexionBD = new ConexionBD();
